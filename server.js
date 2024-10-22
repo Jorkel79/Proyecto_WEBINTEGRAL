e21c743d08d3a79efbfd637a0593c642172fdb17
JIELSAN <jesussjg811@gmail.com>
2024-10-22 00:14:28 -0600

    creacion del servidor http

---
+++ server.js
@@ -0,0 +1,22 @@
+//configurar express
+const express = require('express');
+
+//configurar bodyParser
+const bodyParser = require('body-parser');
+
+//importar el archivo
+
+
+const app = express();
+
+//middlewares
+app.use(bodyParser.json());//se establece el formato de los datos
+
+//agregar añ middleware el objeto
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`servidor escuchando en el puerto ${PORT}`);
+});
