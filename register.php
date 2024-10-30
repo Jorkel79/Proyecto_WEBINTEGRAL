2a14e2683cd1062cc6f8d4ebb6f783c8bd18c173
Jose Salinas <jascjasc081@gmail.com>
2024-10-29 19:55:55 -0600

    agregacionDePaginaDeRegistroYRedireccionDesdeLogin

--- register.php
+++ register.php
@@ -1,28 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/register.css">
 </head>
-
 <body>
     <section class="left-form">
-
-    </section>
-    <section class="right-form">
-
-        <form action="" id="loginForm" method="POST">
-
+        <form action="" id="registerForm" method="POST">
             <img src="css/secupasslogo.png" alt="Logo" class="mobile-logo">
-
 
             <h1>SECUPASS</h1>
 
-            <h2>Login</h2>
+            <h2>Register</h2>
 
             <label for="user">Username</label>
             <input type="text" name="username" id="username" required>
@@ -30,18 +22,17 @@
             <label for="pwd">Password</label>
             <input type="password" name="password" id="password" required>
 
+            <label for="pwd">Correo</label>
+            <input type="email" name="correo" id="correo" required>
+
             <div class="check">
-                <a href="./register.php">Registrarse</a></li>
+                <a href="./login.php">Iniciar Sesion</a></li>
             </div>
-
-            <input type="submit" value="ingresar" name="entrar">
-
+            <input type="submit" value="Registrar" name="entrar">
         </form>
-
     </section>
-
-    <script src="js/login.js"></script>
-
+    <section class="right-form">
+    </section>
 </body>
 
 </html>
