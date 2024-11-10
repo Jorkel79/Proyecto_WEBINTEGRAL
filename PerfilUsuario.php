e04218a62f3f8153f6bebe8db62e6d22ce8bb6e3
Afuo <dsm21190435.aurrea@alumnos.utsv.edu.mx>
2024-11-10 14:38:54 -0600

    archivo para la interfaz de perfil de usuario.

---
+++ PerfilUsuario.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="PerfilUsuario.css">
+</head>
+<body>
+<div>
+<header>
+        <img src="css/secupasslogo.png" alt="Logo">
+        <h1>Perfil De Usuario</h1>
+        <div class="header-buttons">
+            <a href="#">Inicio</a>
+            <a href="#" class="exit-button">Salir</a>
+        </div>
+    </header>
+</div>
+<div class="tarjeta">
+    <div class="foto">
+      <img src="fotoUsuario.png" alt="Foto del usuario">
+    </div>
+    <div class="contenido">
+      <h2 class="nombre">Nombre de Usuario</h2>
+      <p class="correo">correo@ejemplo.com</p>
+      <p class="contraseña">Contraseña: ••••••••</p>
+    </div>
+  </div>
+</body>
+</html>
