13ca086d56b9385a5a84f9eed255b8b704800068
Jorge Raul <valenciasantosjorgeraul@gmail.com>
2024-11-18 20:44:06 -0600

    corrección para que inicie con el login

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    hola
-</body>
-
-</html>
+<?php
+header("Location: login.php");
+exit;
+?>
