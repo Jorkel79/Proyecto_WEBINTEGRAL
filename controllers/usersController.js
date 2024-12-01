f01db6a72b41203d67a8f3661a9b8da8fa447f97
Da_Internet <diego-canto@hotmail.com>
2024-12-01 00:14:22 -0600

    Generacion de Token durante Login y controlador de Cierre de Sesion

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,31 +1,42 @@
 const connection = require('../models/database');
 const db = require('../models/database');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 //LOGIN
-exports.login = async (req, res) => {
+login = async (req, res) => {
     const { username, password } = req.body;
 
     const query = 'SELECT * FROM `usuarios` WHERE username =?;';
 
-    //Verificar las credenciales de un usuario y responde en consecuencia.
+    // Verificar las credenciales de un usuario y responde en consecuencia.
     db.query(query, [username], async (err, results) => {
         if (err) {
             console.error('Error en la consulta:', err);
             return res.status(500).json({ message: "Error en el servidor" });
         }
-        console.log('Resultados de la consulta:', results); //Esto te ayudará a ver qué está devolviendo la base de datos
-        if (results.length > 0) {
 
-            const user = results[0];//Recuperar los datos del usuario para comparar las contraseñas
+        if (results.length > 0) {
+            const user = results[0]; // Recuperar los datos del usuario para comparar las contraseñas
             const esIgual = bcryptjs.compareSync(password, user.password); // Comparar contraseñas
 
             if (esIgual) {
                 console.log("Usuario autenticado correctamente.");
-                res.json({ message: "Login Exitoso", data: results });
-                // Generar un token aquí
+
+                // Generar un token JWT 
+                const token = jwt.sign(
+                    { id: user.id, username: user.username }
+                );
+
+                // Enviar el token al cliente
+                res.json({
+                    message: "Login Exitoso",
+                    data: results,
+                    token: token  // Envía el token al cliente
+                });
             } else {
                 console.log("Contraseña incorrecta.");
-                res.json({ message: "Usuario o contraseña incorrectos", data: [] });
+                res.status(401).json({ message: "Usuario o contraseña incorrectos", data: [] });
             }
         } else {
             res.status(401).json({ message: "Usuario o contraseña incorrectos", data: [] });
@@ -35,7 +46,7 @@ exports.login = async (req, res) => {
 
 
 //REGISTRO
-exports.register = async (req, res) => {
+register = async (req, res) => {
     const { username, password, correo, validacion } = req.body;
 
     //Encriptar la contraseña antes de almacenarla en la base de datos
@@ -66,3 +77,22 @@ exports.register = async (req, res) => {
         });
     });
 }
+
+// LOGOUT
+logout = async (req, res) => {
+
+    if(err){
+        console.log("Error en el cierre de Sesion")
+    }
+
+    res.json({
+        message: "Cierre de Sesion Exitoso"
+    });
+};
+
+
+module.exports = {
+    login,
+    register,
+    logout
+};
